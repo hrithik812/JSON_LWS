8926d23b2ec46f74a1af1feb762452a20a01a83b
hrithik812 <hrithik08.rudra@gmail.com>
2023-04-24 12:25:39 +0600

    JSON first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//JSON--->A particular formatted text
+// It is used for storing & transporting data
+//Mostly used in API communication
+const person={
+    name:"Sumit",
+    age:26
+}
+console.log(person);
